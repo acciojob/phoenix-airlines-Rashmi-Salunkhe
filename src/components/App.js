726b098f6a76c3e1a65dfe4366695d492d2f8b90
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-11-16 00:08:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import LandingPage from '../pages/LandingPage';
+import FlightSearch from '../pages/FlightSearch';
+import FlightBooking from '../pages/FlightBooking';
+import Confirmation from '../pages/Confirmation';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/flight-search">Search Flights</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/flight-search" element={<FlightSearch />} />
+        <Route path="/flight-booking" element={<FlightBooking />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
